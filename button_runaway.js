2c4c92b802aea21cb77e0cd43f43be4cbb6a5d3d
saba <kona.nike3857@gmail.com>
2023-11-27 16:38:35 +0900

    button_runaway

---
+++ button_runaway.js
@@ -0,0 +1,40 @@
+button = document.querySelector("#id_btn")
+const div  = document.querySelector("#id_div")
+const caption = document.querySelector("#id_cap")
+
+var pos = button.getBoundingClientRect();
+
+var x = pos.left;
+var y = pos.top;
+document.addEventListener("mousemove", runaway);
+
+console.log(button.style.backgroundColor)
+
+function rand() {
+	var rand = Math.random();
+	var rand2;
+	if(rand<0.5){
+		rand2 = -1*rand*100-10;
+	}
+	else{
+		rand2 = (rand-0.5)*100+10;
+	}
+	return rand2;
+}
+            
+function runaway(event){
+	if (x-5 < event.clientX && event.clientX < x + 85 && y-5 < event.clientY && event.clientY < y + 45){
+		//button.style.top = event.clientY + "px";
+		//button.style.left = event.clientX + "px";
+		button.style.top = event.clientY +rand()+ "px";
+		button.style.left = event.clientX + rand() + "px";
+		button.style.backgroundColor = "red"
+		console.log(button.style.backgroundColor)
+		pos = button.getBoundingClientRect();
+		x = pos.left;
+		y = pos.top;
+	}
+	else{
+		button = document.querySelector("#id_btn")
+	}
+}
